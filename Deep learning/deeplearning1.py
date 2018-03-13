159f4228bf701e76b4417b95edf26a7fef93f60c
AnanthKumarVasamsetti <ananth.vasamsetti@kony.com>
2018-03-13 16:17:51 +0530

    Added backpropagation

--- Deep learning/deeplearning1.py
+++ Deep learning/deeplearning1.py
@@ -53,11 +53,11 @@ def linear_forward(A, W, b):
 def linear_activation_forward(A_prev, W, b, activation):
     if activation == "sigmoid":
         Z, linear_cache = linear_forward(A_prev, W, b)
-        A, activation_cache = sigmoid(Z)
+        A, activation_cache = sigmoid(Z)    #activation_cache used for backpropagation
     
     elif activation == "relu":
         Z, linear_cache = linear_forward(A_prev, W, b)
-        A, activation_cache = relu(Z)
+        A, activation_cache = relu(Z)       #activation_cache used for backpropagation
     
     assert(A.shape == (W.shape[0], A_prev.shape[1]))
     #Used in backpropagation
@@ -96,10 +96,65 @@ def compute_cost(AL, Y):
     cost = np.squeeze(cost)
     return cost
 
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1/m * np.dot(dZ, A_prev.T)
+    db = 1/m * np.sum(dZ, axis = 1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if(activation == "relu"):
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    elif(activation == "sigmoid"):
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    
+    return dA_prev, dW, db
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    #Reshaping Y into the shape of AL
+    Y = Y.reshape(AL.shape)
+    m = AL.shape[1]
+
+    dAL = -(np.divide(Y, AL) - np.divide(1-Y, 1-AL))
+    current_cache = caches[L - 1]
+
+    grads["dA"+str(L)], grads["dW"+str(L)], grads["db"+str(L)] = linear_activation_backward(dAL, current_cache, activation = "sigmoid")
+
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA"+str(l+2)], current_cache, activation = "relu")
+        grads["dA"+str(l + 1)] = dA_prev_temp
+        grads["dW"+str(l + 1)] = dW_temp
+        grads["db"+str(l + 1)] = db_temp
+    
+    return 
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+
+    for l in range(L):
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * grads["dW" + str(l+1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l+1)]
+    
+    return parameters
+
 def main():
-    Y, AL = compute_cost_test_case()
+    parameters, grads = update_parameters_test_case()
+    parameters = update_parameters(parameters, grads, 0.1)
 
-    print("cost = " + str(compute_cost(AL, Y)))
-
+    print ("W1 = "+ str(parameters["W1"]))
+    print ("b1 = "+ str(parameters["b1"]))
+    print ("W2 = "+ str(parameters["W2"]))
+    print ("b2 = "+ str(parameters["b2"]))
 if __name__ == "__main__":
     main()
